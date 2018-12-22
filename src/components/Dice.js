66fb95362180492065aa1f68bbb55ecf057ba304
Dustch <ivnborgognoni@gmail.com>
2018-12-22 14:57:14 -0300

    more css

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react'
 import './Dicent.css'
 
 class Dice extends Component{
+    
     render(){
         return(
-            <div className="Dice">
+            <div className="Dice" onClick={this.props.clickhandler}>
 
             </div>
         )
--- src/components/Dicent.js
+++ src/components/Dicent.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react'
 import './Dicent.css'
 
-import Dice from './Dice'
+import Dice from './Dice';
 
 class Dicent extends Component{
+
+
+    function(){
+        console.log("hola")
+    }
+    function2(){
+        console.log("chau")
+    }
+
     render(){
         return(
             <div className="Dicent">
                 <div className="dice-container">
+                    <Dice clickhandler={this.function}/>
+                    <Dice clickhandler={this.function2}/>
                     <Dice/>
                     <Dice/>
                     <Dice/>
                     <Dice/>
-                    <Dice/>
-                    <Dice/>
+                </div>
+                <div className="display-container">
+
                 </div>
 
             </div>
