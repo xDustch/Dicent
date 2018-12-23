4513e43c429ba8b0a609b33c0aa147d58bfaa523
Dustch <ivnborgognoni@gmail.com>
2018-12-22 22:25:11 -0300

    Updates

---
+++ precache-manifest.884079801428937ebb68689a5527acb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56d76775861354b6bb19",
+    "url": "/Dicent/static/js/runtime~main.56d76775.js"
+  },
+  {
+    "revision": "c9c2f99f8fb6de388704",
+    "url": "/Dicent/static/js/main.c9c2f99f.chunk.js"
+  },
+  {
+    "revision": "cdde7492564aae05e4bc",
+    "url": "/Dicent/static/js/1.cdde7492.chunk.js"
+  },
+  {
+    "revision": "c9c2f99f8fb6de388704",
+    "url": "/Dicent/static/css/main.e8e8c5a3.chunk.css"
+  },
+  {
+    "revision": "6459baa82b5e521f6023fc1d5c4f5d2e",
+    "url": "/Dicent/index.html"
+  }
+];
